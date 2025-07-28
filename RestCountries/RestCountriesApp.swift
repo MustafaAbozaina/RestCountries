29fbb61cb8f3b793c651fe2254d2c3e03c412ef4
Mustafa Abozaina <MustafaAbozaina@gmail.com>
2025-07-28 19:52:49 +0300

    fix: Prevent CountriesSearchViewModel recreation by moving its initialization to App init

--- RestCountries/RestCountriesApp.swift
+++ RestCountries/RestCountriesApp.swift
@@ -10,12 +10,20 @@ import SwiftData
 
 @main
 struct RestCountriesApp: App {
+    
     @StateObject private var navigationManager = NavigationManager()
-
+    @ObservedObject private var countriesSearchViewModel: CountriesSearchViewModel
+    
+    init() {
+        let navigation = NavigationManager()
+        _navigationManager = StateObject(wrappedValue: navigation)
+        self.countriesSearchViewModel =  CountriesSearchFactory.makeCountriesSearchViewModel(navigationManager: navigation)
+    }
+    
     var body: some Scene {
         WindowGroup {
             Navigator(navigationManager: navigationManager) {
-                CountriesSearchView(viewModel: CountriesSearchFactory.makeCountriesSearchViewModel(navigationManager: navigationManager))
+                CountriesSearchView(viewModel: countriesSearchViewModel)
             }
         }
     }
